f710ce2ee0403ddf3b6e654a04117805e6ba0cdb
mauro <mauro.mls@hotmail.com>
2017-11-09 20:34:37 -0200

    testando a primeira classe de dominio

--- src/main/java/com/silv/resources/CategoriaResource.java
+++ src/main/java/com/silv/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.silv.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.silv.domain.Categoria;
 
 @RestController
 
@@ -10,7 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@GetMapping()
-	public String listar() {
-		return "rest is work";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"informatica");
+		Categoria cat2 = new Categoria(2,"escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
