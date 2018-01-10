d8e739f2c3cc3385b7911c8ca42b749c8dc06373
Mauro Lucio <mauro.mls@hotmail.com>
2018-01-10 17:48:22 -0200

    subindo para o heroku

--- src/main/resources/migration/V1_1_1__CREATE_TABLE_tables.sql
+++ src/main/resources/migration/V1_1_1__CREATE_TABLE_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE categoria
 (
-    id integer NOT NULL DEFAULT,
+    id integer NOT NULL,
     nome character varying(255) COLLATE pg_catalog."default",
     CONSTRAINT categoria_pkey PRIMARY KEY (id)
 )
