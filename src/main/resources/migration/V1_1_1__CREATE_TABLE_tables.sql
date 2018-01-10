d27767259391182c2bdb1a96d86c64fadd174007
Mauro Lucio <mauro.mls@hotmail.com>
2018-01-10 16:23:12 -0200

    subindo para o heroku

--- src/main/resources/migration/V1_1_1__CREATE_TABLE_tables.sql
+++ src/main/resources/migration/V1_1_1__CREATE_TABLE_tables.sql
@@ -1,11 +1,33 @@
-CREATE TABLE plano
-(
-    id uuid NOT NULL,
-    ativo boolean NOT NULL,
-    descricao character varying(255) COLLATE pg_catalog."default",
-    duracao_dias integer,
-    removido boolean NOT NULL,
-    titulo character varying(255) COLLATE pg_catalog."default",
-    valor double precision NOT NULL,
-    CONSTRAINT plano_pkey PRIMARY KEY (id)
-)
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+INSERT INTO `categoria` (`id`, `nome`) VALUES
+(1, 'Informática'),
+(2, 'Escritório'),
+(3, 'Jardinagem'),
+(4, 'Pesca'),
+(5, 'Cama, mesa e banho'),
+(6, 'Alimentação'),
+(7, 'Informática'),
+(8, 'Escritório'),
+(9, 'Jardinagem'),
+(10, 'Pesca'),
+(11, 'Cama, mesa e banho'),
+(12, 'Alimentação'),
+(13, 'Informática'),
+(14, 'Escritório'),
+(15, 'Jardinagem'),
+(16, 'Pesca'),
+(17, 'Cama, mesa e banho'),
+(18, 'Alimentação'),
+(19, 'Informática'),
+(20, 'Escritório'),
+(21, 'Jardinagem'),
+(22, 'Pesca'),
+(23, 'Cama, mesa e banho'),
+(24, 'Alimentação');
